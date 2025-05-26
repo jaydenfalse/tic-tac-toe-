18c29ceb0bd5a40c9b4f517f01f8b3d0c3c2dfcb
Jayden Trieu <jaydentrieuschool@gmail.com>
2025-05-25 20:50:49 -0700

    added move functionality

--- game.h
+++ game.h
@@ -19,6 +19,18 @@ class Board{
         }
     }
 
+    void move(int row, int col, char sym) {
+        if (true = true) { //find a way to check if empty grid
+            waffle[row][col] = sym;
+            cellCount++;
+        }
+        else {
+            cout << "Pick an empty grid." << endl;
+        }
+    }
+
+    bool checkCol{return true} //finish later
+
     private:
     int cellCount;
     char waffle[3][3];
